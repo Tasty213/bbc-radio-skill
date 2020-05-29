4bc17beae3d47693cdb50434149eadf1d9e73b57
George Sykes <gsykes537@gmail.com>
2020-05-29 14:54:25 +0100

    Added the initialisation function

--- __init__.py
+++ __init__.py
@@ -3,6 +3,26 @@ from mycroft import MycroftSkill, intent_file_handler
 
 class BbcRadio(MycroftSkill):
     def __init__(self):
+        # Create a new instance of the VLC player
+        self.instance = vlc.Instance()
+
+        # Create a list containing the URLs of all the radio stations .m3u8 files
+        self.urls = ['http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_high/ak/bbc_radio_one.m3u8',
+        'http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_high/ak/bbc_1xtra.m3u8',
+        'http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_high/ak/bbc_radio_two.m3u8',
+        'http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_high/ak/bbc_radio_three.m3u8',
+        'http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_high/ak/bbc_radio_fourfm.m3u8',
+        'http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_high/ak/bbc_radio_fourlw.m3u8',
+        'http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_high/ak/bbc_radio_four_extra.m3u8',
+        'http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_high/ak/bbc_radio_five_live.m3u8',
+        'http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_high/ak/bbc_radio_five_live_sports_extra.m3u8',
+        'http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_high/ak/bbc_6music.m3u8',
+        'http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_high/ak/bbc_asian_network.m3u8']
+
+        # Create a new instance of the VLC media player
+        self.player = vlc.MediaPlayer()
+
+        # Initialise the Skills library
         MycroftSkill.__init__(self)
 
     @intent_file_handler('radio.bbc.intent')
@@ -10,6 +30,8 @@ class BbcRadio(MycroftSkill):
         self.speak_dialog('radio.bbc')
 
 
+
+
+
 def create_skill():
     return BbcRadio()
-
